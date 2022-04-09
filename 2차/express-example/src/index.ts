73bccc69f0d257659c2973b55d5f77bf9cfe7a09
HyeokJoon Kong <orijoon98@gmail.com>
2022-04-09 17:28:04 +0900

    [ADD] 2차 세미나 실천과제

---
+++ 2차/express-example/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express(); // express 객체 받아옴
+
+app.use(express.json()); // express에서 request body를 json으로 받아오겠다.
+
+app.use('/api', require('./api')); // use -> 모든 요청
+// localhost:8000/api -> api 폴더
+// localhost:8000/api/user -> user.ts 파일
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('Hi! My name is HyeokJoon!');
+}); // get -> http method
+
+app.listen('8000', () => {
+  console.log(`
+        #############################################
+            🛡️ Server listening on port: 8000 🛡️
+        #############################################
+    `);
+}); // 8000 번 포트에서 서버를 실행하겠다 ~
