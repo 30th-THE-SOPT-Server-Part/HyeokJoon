3eb8e45eb2ff64b041bc2a08ae410bd32807ef02
HyeokJoon Kong <orijoon98@gmail.com>
2022-04-09 21:29:11 +0900

    [ADD] 2차 세미나 발전과제

--- 2차/express-example/src/api/index.ts
+++ 2차/express-example/src/api/index.ts
@@ -3,5 +3,8 @@ import express, { Router } from 'express';
 const router: Router = express.Router();
 
 router.use('/user', require('./user'));
+router.use('/blog', require('./blog'));
+router.use('/signup', require('./signup'));
+router.use('/like', require('./like'));
 
 module.exports = router; // 모듈로 반환
