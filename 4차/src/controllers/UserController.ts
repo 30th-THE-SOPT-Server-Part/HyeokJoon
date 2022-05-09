dfd8845cf5f358aedc56f1c57c0b3b1d9c0eb5fc
HyeokJoon Kong <orijoon98@gmail.com>
2022-05-09 09:44:23 +0900

    [ADD] 4차 세미나 발전과제 - express validator 붙이기

--- 4차/src/controllers/UserController.ts
+++ 4차/src/controllers/UserController.ts
@@ -5,6 +5,7 @@ import message from "../modules/responseMessage";
 import util from "../modules/util";
 import { UserService } from "../services";
 import { UserUpdateDto } from "../interfaces/user/UserUpdateDto";
+import { validationResult } from "express-validator";
 
 /**
  *  @route POST /user
@@ -12,6 +13,12 @@ import { UserUpdateDto } from "../interfaces/user/UserUpdateDto";
  *  @access Public
  */
 const createUser = async (req: Request, res: Response) => {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+        return res
+            .status(statusCode.BAD_REQUEST)
+            .send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+    }
     const userCreateDto: UserCreateDto = req.body; // User Create Dto 로 req.body 받아옴
 
     try {
--- 4차/src/routes/UserRouter.ts
+++ 4차/src/routes/UserRouter.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
 import { UserController } from "../controllers";
+import { body } from "express-validator/check";
 
 const router: Router = Router();
 
-router.post("/", UserController.createUser);
+router.post(
+    "/",
+    [
+        body("name").notEmpty(),
+        body("phone").notEmpty(),
+        body("email").notEmpty().isEmail(),
+    ],
+    UserController.createUser
+);
 router.put("/:userId", UserController.updateUser);
 router.get("/:userId", UserController.findUserById);
 router.delete("/:userId", UserController.deleteUser);
